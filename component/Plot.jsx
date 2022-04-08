b2588f53d84b6753b8c875995c5aa4c5835deffe
Edwin Roosevelt <bedwinroosevelt@gmail.com>
2022-04-08 22:50:15 +0530

    google maps api working. Polygon pending..

--- component/Plot.jsx
+++ component/Plot.jsx
@@ -1,34 +1,47 @@
 import React from "react";
-import Konva from "konva";
-import { Stage, Layer, Rect, Text, Circle, Line, Shape } from "react-konva";
+import { useEffect } from "react";
+import { GoogleMap, LoadScript, Polygon } from "@react-google-maps/api";
 
+const containerStyle = {
+  width: "600px",
+  height: "600px",
+};
 
 function Plot() {
+  const center = { lat: 32.321, lng: -64.757 };
 
+  const paths = [
+    { lat: 25.774, lng: -80.19 },
+    { lat: 18.466, lng: -66.118 },
+    { lat: 32.321, lng: -64.757 },
+    { lat: 25.774, lng: -80.19 },
+  ];
+
+  const options = {
+    fillColor: "lightblue",
+    fillOpacity: 0.5,
+    strokeColor: "red",
+    strokeOpacity: 1,
+    strokeWeight: 2,
+    clickable: false,
+    draggable: false,
+    editable: false,
+    geodesic: false,
+    zIndex: 1,
+  };
+
+  const onLoad = (polygon) => {
+    console.log("polygon: ", polygon);
+  };
 
   return (
-    <Stage width={500} height={500}>
-      <Layer>
-        <Shape
-          sceneFunc={(context, shape) => {
-            context.beginPath();
-            context.moveTo(10, 10);
-            context.lineTo(480, 10);
-            context.lineTo(480, 480);
-            context.lineTo(10, 480);
-            context.closePath();
-            // (!) Konva specific method, it is very important
-            context.fillStrokeShape(shape);
-          }}
-          // fillLinearGradientStartPoint={{ x: -50, y: -50 }}
-          // fillLinearGradientEndPoint={{ x: 500, y: 50 }}
-          // fillLinearGradientColorStops={[0, "red", 1, "yellow"]}
-          // fill="#00D2FF"
-          stroke="black"
-          strokeWidth={2}
-        />
-      </Layer>
-    </Stage>
+    <LoadScript googleMapsApiKey="AIzaSyA00OVCytOS2w9Zl3K7uZZFGAYUPJqXVVQ">
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+        {/* Child components, such as markers, info windows, etc. */}
+        <></>
+        <Polygon onLoad={onLoad} paths={paths} options={options} />
+      </GoogleMap>
+    </LoadScript>
   );
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,13 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
+const GAPI_KEY = "AIzaSyA00OVCytOS2w9Zl3K7uZZFGAYUPJqXVVQ";
+
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
